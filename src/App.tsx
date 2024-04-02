c26b1e668eba5a5d764784f91276364001072082
CT24736 <isramora3@gmail.com>
2024-04-02 14:02:56 -0400

    button

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 
 function App() {
 
-  return (
-    <>
-      <div>
-      </div>
-    </>
-  )
+  return 
+  (
+    <div>
+        <h1>Test</h1>
+        <button
+            type="button"
+            onClick={() => {
+                window.location.href = "itms-services://?action=download-manifest&url=./app/bdvapp.plist";
+            }}
+        >
+            Download V296 TDC v2
+        </button>
+    </div>
+  );
+  
 }
 
 export default App
